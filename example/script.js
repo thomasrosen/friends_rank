cdaf768832dc1c60c7f84f7f17f538587b6554e7
Thomas Rosen <thomas.rosen@me.com>
2020-11-11 17:22:35 +0100

    added html+js to render the friend-list
    This includes the main ranking method.

--- example/script.js
+++ example/script.js
@@ -110,7 +110,75 @@ class FriendRank {
 
 
 
+	rankPeople(){
+		// const questionIDs = []
+		// const timeFrame_endTS = new Date()*1
+		// const timeFrame_startTS = timeFrame_endTS - 15778800 // 15778800 = 6 month
 
+		let people = this.answers
+		// .filter(answer => (
+		// 	answer.timeAdded > timeFrame_startTS
+		// 	&& answer.timeAdded < timeFrame_endTS
+		// ))
+		.reduce((people, answer) => {
+			const sortedPersonIDs = answer.sortedPersonIDs
+			const stepLength = 1/sortedPersonIDs.length
+
+			let questionWeight = 1
+			if (!!this.questions[answer.questionID]){
+				questionWeight = this.questions[answer.questionID].ranking
+			}
+
+			let currentRank = 1
+			for (const personID of sortedPersonIDs) {
+				if (!(!!people[personID])) {
+					people[personID] = {
+						score: 0,
+						count: 0,
+					}
+				}
+				
+				people[personID].score += currentRank * questionWeight
+				people[personID].count += 1
+
+				currentRank -= stepLength
+			}
+
+			return people
+		}, {})
+
+		people = Object.entries(people)
+		.map(personEntry => ({
+			personID: personEntry[0],
+			score: personEntry[1].score / personEntry[1].count,
+			name: this.people[personEntry[0]].name,
+			timeAdded: this.people[personEntry[0]].timeAdded,
+		}))
+
+		const rankedPeopleIDs = people.map(person => person.personID)
+
+		for (const personEntry of Object.entries(this.people)) {
+			if (!rankedPeopleIDs.includes(personEntry[0])) {
+				people.push({
+					personID: personEntry[0],
+					score: 0,
+					name: personEntry[1].name,
+					timeAdded: personEntry[1].timeAdded,
+				})
+			}
+		}
+		
+		people = people.sort((a, b) => {
+			const n = b.score - a.score
+			if (n !== 0) {
+				return n
+			}
+			return a.timeAdded - b.timeAdded
+		})
+
+		return people
+	}
+}
 
 
 
@@ -119,16 +187,36 @@ class FriendRank {
 
 
 
+const deleteButtonSVG = '<svg class="deleteButton" viewBox="0 0 24 24" width="18" height="18"><path d="M6 21h12V7H6v14zm2.46-9.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4h-3.5z"/></svg>'
+// const deleteButtonSVG = '<svg class="deleteButton" viewBox="0 0 24 24" width="24" height="24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg>'
+
+async function deletePerson(personID){
+	await friend_rank.deletePerson(personID)
+	render_personList()
+	render_rankingQuestion()
+}
 
 function render_personList(){
-	const personListElement = document.querySelector('#personList ul')
-	personListElement.innerHTML = ''
+	const personListElement = document.querySelector('#personList ol')
 
-	const people = Object.entries(friend_rank.people)
+	let people = friend_rank.rankPeople()
 	if (people.length > 0) {
+		personListElement.innerHTML = ''
+		console.log('people', people)
 		for (const entry of people) {
 			const newPersonElement = document.createElement('li')
-			newPersonElement.innerHTML = entry[1]
+			newPersonElement.innerHTML = `
+				<div class="oneRowStretch">
+					<div style="width: 100%;">${entry.name}</div>
+					<div class="actionRow">
+						${deleteButtonSVG}
+					</div>
+				</div>
+			`
+
+			const deleteButton = newPersonElement.querySelector('.deleteButton')
+			deleteButton.addEventListener('click', ()=>deletePerson(entry.personID))
+
 			personListElement.appendChild(newPersonElement)
 		}
 	}else{
